bc09d25e0a7274ec975e881f090e1684d5203955
Aman Saraf <amansaraf28@gmail.com>
2023-05-03 02:23:03 -0400

    Carousel Padding

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "./carousel.styles.scss";
 
 import img1 from "../../assets/carousle/holi-2.jpg";
 import img2 from "../../assets/carousle/committee-pic.JPG";
